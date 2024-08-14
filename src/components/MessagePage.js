f9b5d60390687a5c14aa8bb68e6e52a504cecfc3
pratikmehakare <pratikmehakare8.com>
2024-08-14 13:55:06 +0530

    msgpage

--- src/components/MessagePage.js
+++ src/components/MessagePage.js
@@ -13,7 +13,7 @@ import Loading from "../components/Spinner";
 import backgroundImage from "../utils/wallapaper.jpeg";
 import { IoMdSend } from "react-icons/io";
 import moment from "moment";
-import EditUserDetails from './EditUserDetails';
+import EditUserDetails from "./EditUserDetails";
 import chatapp from "../utils/chatapp.png";
 
 const MessagePage = () => {
@@ -38,7 +38,7 @@ const MessagePage = () => {
   const [loading, setLoading] = useState(false);
   const [allMessage, setAllMessage] = useState([]);
   const currentMessage = useRef(null);
-  const [editUserOpen,setEditUserOpen] = useState(false)
+  const [editUserOpen, setEditUserOpen] = useState(false);
 
   useEffect(() => {
     if (currentMessage.current) {
@@ -121,7 +121,7 @@ const MessagePage = () => {
   }, [socketConnection, params?.userId, user]);
 
   const handleOnChange = (e) => {
-    const {  value } = e.target;
+    const { value } = e.target;
 
     setMessage((preve) => {
       return {
@@ -153,212 +153,208 @@ const MessagePage = () => {
     }
   };
 
-
   return (
     <div
-      style={{ backgroundImage: `url(${backgroundImage})` }}
-      className="bg-no-repeat bg-cover"
-    >
-      <header className="sticky top-0 h-16 bg-white flex justify-between items-center px-4">
-        <div className="flex items-center gap-4">
-          <Link to={"/home"} className="lg:hidden">
-            <FaAngleLeft size={25} />
-          </Link>
-          <div>
-            <Avatar
-              width={50}
-              height={50}
-              imageUrl={dataUser?.profile_pic}
-              name={dataUser?.name}
-              userId={dataUser?._id}
+    style={{ backgroundImage: `url(${backgroundImage})` }}
+    className="bg-no-repeat bg-cover"
+  >
+    <header className="sticky top-0 h-16 sm:h-12 bg-white flex justify-between items-center px-4 sm:px-2">
+      <div className="flex items-center gap-4">
+        <Link to={"/home"} className="lg:hidden">
+          <FaAngleLeft size={25} />
+        </Link>
+        <div>
+          <Avatar
+            width={50}
+            height={50}
+            imageUrl={dataUser?.profile_pic}
+            name={dataUser?.name}
+            userId={dataUser?._id}
+          />
+        </div>
+        <div>
+          <h3 className="font-semibold text-lg my-0 text-ellipsis line-clamp-1">
+            {dataUser?.name}
+          </h3>
+          <p className="-my-2 text-sm">
+            {dataUser.online ? (
+              <span className="text-primary">online</span>
+            ) : (
+              <span className="text-slate-400">offline</span>
+            )}
+          </p>
+        </div>
+      </div>
+  
+      <div className="flex justify-center items-center">
+        <img src={chatapp} width={40} height={40} alt="logo" />
+        <h3 className="flex justify-center items-center text-4xl font-semibold sm:text-2xl">
+          QuickChat
+        </h3>
+      </div>
+  
+      <div>
+        <button onClick={()=>setEditUserOpen(true)} className="cursor-pointer hover:text-primary">
+          <HiDotsVertical />
+        </button>
+      </div>
+    </header>
+  
+    {/***show all messages */}
+    <section className="h-[calc(100vh-128px)] sm:h-[calc(100vh-100px)] overflow-x-hidden overflow-y-scroll scrollbar relative bg-slate-200 bg-opacity-50">
+      {/**all messages shown here */}
+      <div className="flex flex-col gap-2 py-2 sm:py-1 mx-2 sm:mx-1" ref={currentMessage}>
+        {allMessage.map((msg, index) => {
+          return (
+            <div
+              className={`p-1 py-1 rounded w-fit max-w-[280px] sm:max-w-[220px] md:max-w-sm lg:max-w-md ${
+                user._id === msg?.msgByUserId ? "ml-auto bg-teal-100" : "bg-white"
+              }`}
+              key={index}
+            >
+              <div className="w-full relative">
+                {msg?.imageUrl && (
+                  <img
+                    src={msg?.imageUrl}
+                    className="w-full h-full object-scale-down"
+                    alt="img"
+                  />
+                )}
+                {msg?.videoUrl && (
+                  <video
+                    src={msg.videoUrl}
+                    className="w-full h-full object-scale-down"
+                    controls
+                  />
+                )}
+              </div>
+              <p className="px-2">{msg.text}</p>
+              <p className="text-xs ml-auto w-fit">
+                {moment(msg.createdAt).format("hh:mm")}
+              </p>
+            </div>
+          );
+        })}
+      </div>
+  
+      {/**upload Image display */}
+      {message.imageUrl && (
+        <div className="w-full h-full sticky bottom-0 bg-slate-700 bg-opacity-30 flex justify-center items-center rounded overflow-hidden">
+          <div
+            className="w-fit p-2 absolute top-0 right-0 cursor-pointer hover:text-red-600"
+            onClick={handleClearUploadImage}
+          >
+            <IoClose size={30} />
+          </div>
+          <div className="bg-white p-3">
+            <img
+              src={message.imageUrl}
+              alt="uploadImage"
+              className="aspect-square w-full h-full max-w-sm m-2 object-scale-down"
             />
           </div>
-          <div>
-            <h3 className="font-semibold text-lg my-0 text-ellipsis line-clamp-1">
-              {dataUser?.name}
-            </h3>
-            <p className="-my-2 text-sm">
-              {dataUser.online ? (
-                <span className="text-primary">online</span>
-              ) : (
-                <span className="text-slate-400">offline</span>
-              )}
-            </p>
-          </div>
         </div>
-
-        <div className="flex justify-center items-center">
-          <img src={chatapp} width={40} height={40} alt="logo" />
-          <h3 className="flex justify-center items-center text-4xl font-semibold">
-            QuickChat
-          </h3>
-        </div>
-
-        <div>
-          <button onClick={()=>setEditUserOpen(true)} className="cursor-pointer hover:text-primary">
-            <HiDotsVertical />
-          </button>
-        </div>
-      </header>
-
-      {/***show all message */}
-      <section className="h-[calc(100vh-128px)] overflow-x-hidden overflow-y-scroll scrollbar relative bg-slate-200 bg-opacity-50">
-        {/**all message show here */}
-        <div className="flex flex-col gap-2 py-2 mx-2" ref={currentMessage}>
-          {allMessage.map((msg, index) => {
-            return (
-              <div
-                className={` p-1 py-1 rounded w-fit max-w-[280px] md:max-w-sm lg:max-w-md ${
-                  user._id === msg?.msgByUserId
-                    ? "ml-auto bg-teal-100"
-                    : "bg-white"
-                }`}
-              >
-                <div className="w-full relative">
-                  {msg?.imageUrl && (
-                    <img
-                      src={msg?.imageUrl}
-                      className="w-full h-full object-scale-down"
-                      alt="img"
-                    />
-                  )}
-                  {msg?.videoUrl && (
-                    <video
-                      src={msg.videoUrl}
-                      className="w-full h-full object-scale-down"
-                      controls
-                    />
-                  )}
-                </div>
-                <p className="px-2">{msg.text}</p>
-                <p className="text-xs ml-auto w-fit">
-                  {moment(msg.createdAt).format("hh:mm")}
-                </p>
-              </div>
-            );
-          })}
-        </div>
-
-        {/**upload Image display */}
-        {message.imageUrl && (
-          <div className="w-full h-full sticky bottom-0 bg-slate-700 bg-opacity-30 flex justify-center items-center rounded overflow-hidden">
-            <div
-              className="w-fit p-2 absolute top-0 right-0 cursor-pointer hover:text-red-600"
-              onClick={handleClearUploadImage}
-            >
-              <IoClose size={30} />
-            </div>
-            <div className="bg-white p-3">
-              <img
-                src={message.imageUrl}
-                alt="uploadImage"
-                className="aspect-square w-full h-full max-w-sm m-2 object-scale-down"
-              />
-            </div>
-          </div>
-        )}
-
-        {/**upload video display */}
-        {message.videoUrl && (
-          <div className="w-full h-full sticky bottom-0 bg-slate-700 bg-opacity-30 flex justify-center items-center rounded overflow-hidden">
-            <div
-              className="w-fit p-2 absolute top-0 right-0 cursor-pointer hover:text-red-600"
-              onClick={handleClearUploadVideo}
-            >
-              <IoClose size={30} />
-            </div>
-            <div className="bg-white p-3">
-              <video
-                src={message.videoUrl}
-                className="aspect-square w-full h-full max-w-sm m-2 object-scale-down"
-                controls
-                muted
-                autoPlay
-              />
-            </div>
-          </div>
-        )}
-
-        {loading && (
-          <div className="w-full h-full flex sticky bottom-0 justify-center items-center">
-            <Loading />
-          </div>
-        )}
-      </section>
-
-      {/**send message */}
-      <section className="h-16 bg-white flex items-center px-4">
-        <div className="relative ">
-          <button
-            onClick={handleUploadImageVideoOpen}
-            className="flex justify-center items-center w-11 h-11 rounded-full hover:bg-primary hover:text-white"
+      )}
+  
+      {/**upload video display */}
+      {message.videoUrl && (
+        <div className="w-full h-full sticky bottom-0 bg-slate-700 bg-opacity-30 flex justify-center items-center rounded overflow-hidden">
+          <div
+            className="w-fit p-2 absolute top-0 right-0 cursor-pointer hover:text-red-600"
+            onClick={handleClearUploadVideo}
           >
-            <FaPlus size={20} />
-          </button>
-
-          {/**video and image */}
-          {openImageVideoUpload && (
-            <div className="bg-white shadow rounded absolute bottom-14 w-36 p-2">
-              <form>
-                <label
-                  htmlFor="uploadImage"
-                  className="flex items-center p-2 px-3 gap-3 hover:bg-slate-200 cursor-pointer"
-                >
-                  <div className="text-primary">
-                    <FaImage size={18} />
-                  </div>
-                  <p>Image</p>
-                </label>
-                <label
-                  htmlFor="uploadVideo"
-                  className="flex items-center p-2 px-3 gap-3 hover:bg-slate-200 cursor-pointer"
-                >
-                  <div className="text-purple-500">
-                    <FaVideo size={18} />
-                  </div>
-                  <p>Video</p>
-                </label>
-
-                <input
-                  type="file"
-                  id="uploadImage"
-                  onChange={handleUploadImage}
-                  className="hidden"
-                />
-
-                <input
-                  type="file"
-                  id="uploadVideo"
-                  onChange={handleUploadVideo}
-                  className="hidden"
-                />
-              </form>
-            </div>
-          )}
+            <IoClose size={30} />
+          </div>
+          <div className="bg-white p-3">
+            <video
+              src={message.videoUrl}
+              className="aspect-square w-full h-full max-w-sm m-2 object-scale-down"
+              controls
+              muted
+              autoPlay
+            />
+          </div>
         </div>
-
-        {/**input box */}
-        <form className="h-full w-full flex gap-2" onSubmit={handleSendMessage}>
-          <input
-            type="text"
-            placeholder="Type here message..."
-            className="py-1 px-4 outline-none w-full h-full"
-            value={message.text}
-            onChange={handleOnChange}
-          />
-          <button className="text-primary hover:text-secondary">
-            <IoMdSend size={28} />
-          </button>
-        </form>
-      </section>
-      
-      {
-                editUserOpen && (
-                    <EditUserDetails onClose={()=>setEditUserOpen(false)} user={user}/>
-                )
-            }
-
-    </div>
+      )}
+  
+      {loading && (
+        <div className="w-full h-full flex sticky bottom-0 justify-center items-center">
+          <Loading />
+        </div>
+      )}
+    </section>
+  
+    {/**send message */}
+    <section className="h-16 sm:h-12 bg-white flex items-center px-4 sm:px-2">
+      <div className="relative">
+        <button
+          onClick={handleUploadImageVideoOpen}
+          className="flex justify-center items-center w-11 h-11 sm:w-9 sm:h-9 rounded-full hover:bg-primary hover:text-white"
+        >
+          <FaPlus size={20} />
+        </button>
+  
+        {/**video and image */}
+        {openImageVideoUpload && (
+          <div className="bg-white shadow rounded absolute bottom-14 w-36 p-2">
+            <form>
+              <label
+                htmlFor="uploadImage"
+                className="flex items-center p-2 px-3 gap-3 hover:bg-slate-200 cursor-pointer"
+              >
+                <div className="text-primary">
+                  <FaImage size={18} />
+                </div>
+                <p>Image</p>
+              </label>
+              <label
+                htmlFor="uploadVideo"
+                className="flex items-center p-2 px-3 gap-3 hover:bg-slate-200 cursor-pointer"
+              >
+                <div className="text-purple-500">
+                  <FaVideo size={18} />
+                </div>
+                <p>Video</p>
+              </label>
+  
+              <input
+                type="file"
+                id="uploadImage"
+                onChange={handleUploadImage}
+                className="hidden"
+              />
+  
+              <input
+                type="file"
+                id="uploadVideo"
+                onChange={handleUploadVideo}
+                className="hidden"
+              />
+            </form>
+          </div>
+        )}
+      </div>
+  
+      {/**input box */}
+      <form className="h-full w-full flex gap-2" onSubmit={handleSendMessage}>
+        <input
+          type="text"
+          placeholder="Type here message..."
+          className="py-1 px-4 outline-none w-full h-full"
+          value={message.text}
+          onChange={handleOnChange}
+        />
+        <button className="text-primary hover:text-secondary">
+          <IoMdSend size={28} />
+        </button>
+      </form>
+    </section>
+  
+    {editUserOpen && (
+      <EditUserDetails onClose={() => setEditUserOpen(false)} user={user} />
+    )}
+  </div>
+  
   );
 };
 
