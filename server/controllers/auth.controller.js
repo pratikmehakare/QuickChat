1ff9d85c8d625494103c02b23f81335665e5a5c7
pratikmehakare <pratikmehakare8.com>
2024-08-14 11:08:32 +0530

    bjs

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 const User = require("../models/user.model");
 const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
+const bcrypt = require('bcryptjs');
+
 
 exports.login = async (req, res) => {
   try {
